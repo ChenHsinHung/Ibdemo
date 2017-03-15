29d6f9ef063795820612945eb41fae40eca4fa76
chen hsin hung <chenhsinhung@Chen-hsin-hungteki-MacBook-Pro.local>
2017-03-16 00:01:02 +0800

    Initial Commit

---
+++ IBDesignable_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBDesignable_demo
+//
+//  Created by chen hsin hung on 2017/3/16.
+//  Copyright © 2017年 chen hsin hung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
